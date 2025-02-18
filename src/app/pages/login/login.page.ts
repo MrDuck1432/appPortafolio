2858f1f006164e789a146972057f0937c62bfadb
MrDuck1432 <sebastian_escobedo_dp@outlook>
2025-02-18 11:35:03 -0300

    login agregado

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
   standalone: false,
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario enviado:', this.loginForm.value);
+      // Aquí iría la lógica de autenticación
+    }
+  }
 }
